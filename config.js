9ed0b12d58fd828305a8627d3ac6ffdddccc4d3f
Jintu Das <jintuisbusy@gmail.com>
2018-06-25 15:46:30 +0530

    new ip added

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-var  MAIN_URL = 'http://13.127.81.82';
+var  MAIN_URL = 'http://13.232.61.17';
 
 var config = {
 
-    ADMIN_USERNAME : 'backend',
-    ADMIN_PASSWORD : 'qwerty123',
+    ADMIN_USERNAME : 'new-admin',
+    ADMIN_PASSWORD : '!admin123!',
     INSTAGRAM_ACCESS_TOKEN : '8006095402.8a540c5.df7cf68e50b241c39ea3dd6463452527',
     MEDIA_BASE_URL : MAIN_URL+'/magento/pub/media/catalog/product/',
-    EXCLUSIVE_URL : MAIN_URL+"/magento/rest/V1/products?searchCriteria[filter_groups][0][filters][0][field]=category_id&searchCriteria[filter_groups][0][filters][0][value]=4&searchCriteria[filter_groups][1][filters][0][field]=type_id&searchCriteria[filter_groups][1][filters][0][value]=configurable",
+    EXCLUSIVE_URL : MAIN_URL+"/magento/rest/V1/products?searchCriteria[filter_groups][0][filters][0][field]=category_id&searchCriteria[filter_groups][0][filters][0][value]=2&searchCriteria[filter_groups][1][filters][0][field]=type_id&searchCriteria[filter_groups][1][filters][0][value]=configurable",
     ADMIN_URL : MAIN_URL+'/magento/rest/V1/integration/admin/token',
     PRODUCT_INFO : MAIN_URL+'/magento/rest/V1/products/',
     CATEGORIES_URL : MAIN_URL+'/magento/rest/V1/categories',
-    HOME_CAMP_1 : MAIN_URL+"/magento/rest/V1/products?searchCriteria[filter_groups][1][filters][0][field]=category_id&searchCriteria[filter_groups][1][filters][0][value]=8",
-    HOME_CAMP_2 : MAIN_URL+"/magento/rest/V1/products?searchCriteria[filter_groups][1][filters][0][field]=category_id&searchCriteria[filter_groups][1][filters][0][value]=9",
+    HOME_CAMP_1 : MAIN_URL+"/magento/rest/V1/products?searchCriteria[filter_groups][1][filters][0][field]=category_id&searchCriteria[filter_groups][1][filters][0][value]=7",
+    HOME_CAMP_2 : MAIN_URL+"/magento/rest/V1/products?searchCriteria[filter_groups][1][filters][0][field]=category_id&searchCriteria[filter_groups][1][filters][0][value]=8",
     CATEGORY_CAMP_1 : MAIN_URL+"/magento/rest/V1/products?searchCriteria[filter_groups][1][filters][0][field]=category_id&searchCriteria[filter_groups][1][filters][0][value]=10",
     SEARCH_PRODUCT :  MAIN_URL+"/magento/rest/V1/products?searchCriteria[filter_groups][1][filters][0][field]=category_id&searchCriteria[filter_groups][1][filters][0][value]=",
     CONFIGURABLE_PRODUCTS : "&searchCriteria[filter_groups][1][filters][0][field]=type_id&searchCriteria[filter_groups][1][filters][0][value]=configurable",
